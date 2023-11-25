257a613af2dd8ea4d9d2060b095c72ad2a51fd52
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-11-25 14:04:24 +0300

    add update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
+import { ToastrModule } from "ngx-toastr";
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { HttpClientModule } from "@angular/common/http";
     MaterialModule,
     ReactiveFormsModule,
     HttpClientModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]

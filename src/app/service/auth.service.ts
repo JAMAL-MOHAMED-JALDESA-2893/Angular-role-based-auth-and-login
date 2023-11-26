492186bc14e11ef301da040d3f5025007d92bd3a
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-11-26 23:49:40 +0300

    update

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
-  apiurl=''
+  apiurl='http://localhost:3000/user'
 
   GetAll() {
      return this.http.get(this.apiurl)

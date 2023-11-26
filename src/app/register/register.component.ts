e27e0824b8c72aada8e2757255e8b6dee934f86c
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-11-26 23:34:30 +0300

    update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -15,7 +15,7 @@ export class RegisterComponent {
   registerform = this.builder.group({
     id: this.builder.control('', Validators.compose([Validators.required, Validators.minLength(5)])),
     name: this.builder.control('', Validators.required),
-    password: this.builder.control('', Validators.compose([])),
+    password: this.builder.control('', Validators.compose([Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')])),
     email: this.builder.control('', Validators.compose([Validators.required, Validators.email])),
     gender: this.builder.control('male'),
     isactive: this.builder.control(false)

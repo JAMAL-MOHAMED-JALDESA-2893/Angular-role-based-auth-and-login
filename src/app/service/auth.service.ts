b3fd1722f91a5f284b9051409fe4a4c235e504e4
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-11-27 00:05:23 +0300

    update

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -9,18 +9,22 @@ export class AuthService {
   constructor(private http: HttpClient) { }
   apiurl = 'http://localhost:3000/user';
 
+
+  // fetching for all the data
   GetAll() {
     return this.http.get(this.apiurl);
   }
 
+  // fetch by code or id function
   GetbyCode(code:any) {
     return this.http.get(this.apiurl + "/" + code);
   }
 
+  // posting the registering details function
   Proceedregister(inputdata:any) {
     return this.http.post(this.apiurl, inputdata);
   }
-
+   // updating the register details
   Updateuser(code: any, inputdata: any) {
     return this.http.put(this.apiurl + '/' + code, inputdata);
   }

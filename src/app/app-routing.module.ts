4d06a444189058dbca74dd87d5bfb1d119a08814
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-11-26 17:10:30 +0300

    add update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
@@ -8,10 +9,18 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
     path: '',
     component: HomeComponent
   },
-  
+  {
+    path: '',
+    component: HomeComponent
+  }
+
 ];
 
 @NgModule({
